42b6f4e6104f49c3994ba031c7c4ef6e426e9805
Oniel Santos Álvarez <59784349+Oniel1721@users.noreply.github.com>
2022-01-19 09:26:43 -0400

    debug, prefix and app configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
+  const globalPrefix = "api";
+
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(globalPrefix);
   await app.listen(3000);
 }
 bootstrap();
