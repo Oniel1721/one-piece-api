6aefc2302a577766016b266a30d973b32862dadb
Oniel Santos Álvarez <59784349+Oniel1721@users.noreply.github.com>
2022-01-19 18:21:33 -0400

    swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap () {
   const globalPrefix = 'api'
 
   const app = await NestFactory.create(AppModule)
+  app.enableCors()
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('ONE PIECE API')
+    .setVersion('1.0')
+    .build()
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('docs', app, swaggerDocument)
   app.setGlobalPrefix(globalPrefix)
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(AppModule.port)
