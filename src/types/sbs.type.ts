f0da4461e1c80591c6d2033aa2c589a7f9d50cdf
Oniel Santos Álvarez <59784349+Oniel1721@users.noreply.github.com>
2022-01-20 23:06:48 -0400

    types added to doc

--- src/types/sbs.type.ts
+++ src/types/sbs.type.ts
@@ -1,7 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Image } from './image.type'
 
 export class SBS {
+    @ApiProperty()
     reader: string
+
+    @ApiProperty()
     oda: string
+
+    @ApiProperty({
+      required: false
+    })
     imgUrl: Image | null
 }
